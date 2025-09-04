a28592325f8351ffbc63ef5ae503c3b2392ae9c0
GermanBluefox <dogafox@gmail.com>
2025-09-04 18:40:29 +0200

    chore: release v0.2.2
    - (@GermanBluefox) Added select ID dialog to the package

--- dist/fileHelper.js
+++ dist/fileHelper.js
@@ -1,7 +1,8 @@
-/** @type {HTMLIFrameElement | null} */
-let fileDialog = null;7
-/** @type {(string|null) => void} */
-let storedResolve = null;
+/** @type {((string|null) => void) | null} */
+let storedFileResolve = null;
+/** @type {((string|null) => void) | null} */
+let storedObjectResolve = null;
+let codeLoaded = false;
 /**
  *
  * @param {{
@@ -34,162 +35,124 @@ let storedResolve = null;
  */
 export async function openFileDialog(config) {
     return new Promise(async resolve => {
-        storedResolve = resolve;
-        if (!fileDialog) {
+        storedFileResolve = resolve;
+        if (!codeLoaded) {
             await import('./iobrokerFile.es.js');
-            fileDialog = document.createElement('iobroker-file');
-            window._iobFileDialogOnSelected = (newId) => {
-                fileDialog.setAttribute('open', 'false');
-                storedResolve(newId);
-            };
-            fileDialog.setAttribute('id', 'iob-file');
-            fileDialog.setAttribute('port', (config.port || window.location.port).toString());
-            fileDialog.setAttribute('host', config.host);
-            fileDialog.setAttribute('protocol', config.protocol);
-            fileDialog.setAttribute('language', config.language);
-            fileDialog.setAttribute('onclose', '_iobFileDialogOnSelected');
-            fileDialog.setAttribute('primary', '#AD1625');
-            fileDialog.setAttribute('secondary', 'rgb(228, 145, 145)');
-            fileDialog.setAttribute('paper', 'rgb(243, 243, 243)');
-            fileDialog.setAttribute('selected', config.selected);
-            fileDialog.setAttribute('token', config.token);
-            fileDialog.setAttribute('open', 'true');
-            if (config.selectOnlyFolders !== undefined) {
-                fileDialog.setAttribute('selectonlyfolders', config.selectOnlyFolders ? 'true' : 'false');
-            }
-            if (config.filterByType !== undefined) {
-                fileDialog.setAttribute('filterbytype', config.filterByType);
-            }
-            if (config.filterFiles !== undefined) {
-                fileDialog.setAttribute('filterfiles', config.filterFiles.join(','));
-            }
-            if (config.limitPath !== undefined) {
-                fileDialog.setAttribute('limitpath', config.limitPath);
-            }
-            if (config.imagePrefix !== undefined) {
-                fileDialog.setAttribute('imageprefix', config.imagePrefix);
-            }
-            if (config.showToolbar !== undefined) {
-                fileDialog.setAttribute('showtoolbar', config.showToolbar ? 'true' : 'false');
-            }
-            if (config.allowUpload !== undefined) {
-                fileDialog.setAttribute('allowupload', config.allowUpload ? 'true' : 'false');
-            }
-            if (config.allowDownload !== undefined) {
-                fileDialog.setAttribute('allowdownload', config.allowDownload ? 'true' : 'false');
-            }
-            if (config.allowCreateFolder !== undefined) {
-                fileDialog.setAttribute('allowcreatefolder', config.allowCreateFolder ? 'true' : 'false');
-            }
-            if (config.allowDelete !== undefined) {
-                fileDialog.setAttribute('allowdelete', config.allowDelete ? 'true' : 'false');
-            }
-            if (config.allowView !== undefined) {
-                fileDialog.setAttribute('allowview', config.allowView ? 'true' : 'false');
-            }
-            if (config.showTypeSelector !== undefined) {
-                fileDialog.setAttribute('showtypeselector', config.showTypeSelector ? 'true' : 'false');
-            }
-            if (config.restrictToFolder !== undefined) {
-                fileDialog.setAttribute('restrictofolder', config.restrictToFolder);
-            }
-            if (config.expertMode !== undefined) {
-                fileDialog.setAttribute('expertmode', config.expertMode ? 'true' : 'false');
-            }
-            if (config.showExpertButton !== undefined) {
-                fileDialog.setAttribute('showexpertbutton', config.showExpertButton ? 'true' : 'false');
-            }
-            if (config.allowNonRestricted !== undefined) {
-                fileDialog.setAttribute('allownonrestricted', config.allowNonRestricted ? 'true' : 'false');
-            }
-
-            document.body.appendChild(fileDialog);
-        } else {
-            if (config.selectOnlyFolders !== undefined) {
-                fileDialog.setAttribute('selectonlyfolders', config.selectOnlyFolders ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('selectonlyfolders');
-            }
-            if (config.imagePrefix !== undefined) {
-                fileDialog.setAttribute('imageprefix', config.imagePrefix);
-            } else {
-                fileDialog.removeAttribute('imageprefix');
-            }
-            if (config.filterByType !== undefined) {
-                fileDialog.setAttribute('filterbytype', config.filterByType);
-            } else {
-                fileDialog.removeAttribute('filterbytype');
-            }
-            if (config.filterFiles !== undefined) {
-                fileDialog.setAttribute('filterfiles', config.filterFiles.join(','));
-            } else {
-                fileDialog.removeAttribute('filterfiles');
-            }
-            if (config.limitPath !== undefined) {
-                fileDialog.setAttribute('limitpath', config.limitPath);
-            } else {
-                fileDialog.removeAttribute('limitpath');
-            }
-            if (config.showToolbar !== undefined) {
-                fileDialog.setAttribute('showtoolbar', config.showToolbar ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('showtoolbar');
-            }
-            if (config.allowUpload !== undefined) {
-                fileDialog.setAttribute('allowupload', config.allowUpload ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('allowupload');
-            }
-            if (config.allowDownload !== undefined) {
-                fileDialog.setAttribute('allowdownload', config.allowDownload ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('allowdownload');
-            }
-            if (config.allowCreateFolder !== undefined) {
-                fileDialog.setAttribute('allowcreatefolder', config.allowCreateFolder ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('allowcreatefolder');
-            }
-            if (config.allowDelete !== undefined) {
-                fileDialog.setAttribute('allowdelete', config.allowDelete ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('allowdelete');
-            }
-            if (config.allowView !== undefined) {
-                fileDialog.setAttribute('allowview', config.allowView ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('allowview');
-            }
-            if (config.showTypeSelector !== undefined) {
-                fileDialog.setAttribute('showtypeselector', config.showTypeSelector ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('showtypeselector');
-            }
-            if (config.restrictToFolder !== undefined) {
-                fileDialog.setAttribute('restrictofolder', config.restrictToFolder);
-            } else {
-                fileDialog.removeAttribute('restrictofolder');
-            }
-            if (config.expertMode !== undefined) {
-                fileDialog.setAttribute('expertmode', config.expertMode ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('expertmode');
-            }
-            if (config.showExpertButton !== undefined) {
-                fileDialog.setAttribute('showexpertbutton', config.showExpertButton ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('showexpertbutton');
-            }
-            if (config.allowNonRestricted !== undefined) {
-                fileDialog.setAttribute('allownonrestricted', config.allowNonRestricted ? 'true' : 'false');
-            } else {
-                fileDialog.removeAttribute('allownonrestricted');
-            }
-
-
-            fileDialog.setAttribute('selected', config.selected);
-            fileDialog.setAttribute('token', config.token);
-            fileDialog.setAttribute('open', 'true');
+            codeLoaded = true;
         }
+
+        // destroy old dialog
+        let fileDialog = document.getElementById('iobroker-select-id');
+        if (fileDialog) {
+            fileDialog.remove();
+        }
+
+        fileDialog = document.createElement('iobroker-file');
+        window._iobFileDialogOnSelected = (newId) => {
+            fileDialog.setAttribute('open', 'false');
+            fileDialog.remove();
+            storedFileResolve(newId);
+        };
+        fileDialog.setAttribute('id', 'iob-file');
+        fileDialog.setAttribute('port', (config.port || window.location.port).toString());
+        fileDialog.setAttribute('host', config.host);
+        fileDialog.setAttribute('protocol', config.protocol);
+        fileDialog.setAttribute('language', config.language);
+        fileDialog.setAttribute('onclose', '_iobFileDialogOnSelected');
+        fileDialog.setAttribute('primary', '#AD1625');
+        fileDialog.setAttribute('secondary', 'rgb(228, 145, 145)');
+        fileDialog.setAttribute('paper', 'rgb(243, 243, 243)');
+        fileDialog.setAttribute('selected', config.selected);
+        fileDialog.setAttribute('token', config.token);
+        fileDialog.setAttribute('open', 'true');
+        if (config.selectOnlyFolders !== undefined) {
+            fileDialog.setAttribute('selectonlyfolders', config.selectOnlyFolders ? 'true' : 'false');
+        }
+        if (config.filterByType !== undefined) {
+            fileDialog.setAttribute('filterbytype', config.filterByType);
+        }
+        if (config.filterFiles !== undefined) {
+            fileDialog.setAttribute('filterfiles', config.filterFiles.join(','));
+        }
+        if (config.limitPath !== undefined) {
+            fileDialog.setAttribute('limitpath', config.limitPath);
+        }
+        if (config.imagePrefix !== undefined) {
+            fileDialog.setAttribute('imageprefix', config.imagePrefix);
+        }
+        if (config.showToolbar !== undefined) {
+            fileDialog.setAttribute('showtoolbar', config.showToolbar ? 'true' : 'false');
+        }
+        if (config.allowUpload !== undefined) {
+            fileDialog.setAttribute('allowupload', config.allowUpload ? 'true' : 'false');
+        }
+        if (config.allowDownload !== undefined) {
+            fileDialog.setAttribute('allowdownload', config.allowDownload ? 'true' : 'false');
+        }
+        if (config.allowCreateFolder !== undefined) {
+            fileDialog.setAttribute('allowcreatefolder', config.allowCreateFolder ? 'true' : 'false');
+        }
+        if (config.allowDelete !== undefined) {
+            fileDialog.setAttribute('allowdelete', config.allowDelete ? 'true' : 'false');
+        }
+        if (config.allowView !== undefined) {
+            fileDialog.setAttribute('allowview', config.allowView ? 'true' : 'false');
+        }
+        if (config.showTypeSelector !== undefined) {
+            fileDialog.setAttribute('showtypeselector', config.showTypeSelector ? 'true' : 'false');
+        }
+        if (config.restrictToFolder !== undefined) {
+            fileDialog.setAttribute('restrictofolder', config.restrictToFolder);
+        }
+        if (config.expertMode !== undefined) {
+            fileDialog.setAttribute('expertmode', config.expertMode ? 'true' : 'false');
+        }
+        if (config.showExpertButton !== undefined) {
+            fileDialog.setAttribute('showexpertbutton', config.showExpertButton ? 'true' : 'false');
+        }
+        if (config.allowNonRestricted !== undefined) {
+            fileDialog.setAttribute('allownonrestricted', config.allowNonRestricted ? 'true' : 'false');
+        }
+
+        document.body.appendChild(fileDialog);
+    });
+}
+/**
+ *
+ * @param {{host:string, port: number, protocol: 'http:' | 'https:', language: string, selected?: string, allowAll?: boolean, primary?: string; secondary?: string, paper?: string, token?: string}} config
+ * @returns {Promise<string?>}
+ */
+export async function openSelectIdDialog(config) {
+    return new Promise(async resolve => {
+        storedObjectResolve = resolve;
+        if (!codeLoaded) {
+            await import('./iobrokerFile.es.js');
+            codeLoaded = true;
+        }
+        let selectDialog = document.getElementById('iobroker-select-id');
+        if (selectDialog) {
+            selectDialog.remove();
+        }
+
+        selectDialog = document.createElement('iobroker-select-id');
+        window._iobSelectIdDialogOnSelected = (newId) => {
+            const _selectDialog = document.getElementById('iobroker-select-id');
+            _selectDialog.remove();
+            storedObjectResolve(newId);
+        };
+        selectDialog.setAttribute('id', 'iob-select-id');
+        selectDialog.setAttribute('port', (config.port || window.location.port).toString());
+        selectDialog.setAttribute('host', config.host);
+        selectDialog.setAttribute('protocol', config.protocol);
+        selectDialog.setAttribute('language', config.language);
+        selectDialog.setAttribute('onclose', '_iobSelectIdDialogOnSelected');
+        selectDialog.setAttribute('primary', '#AD1625');
+        selectDialog.setAttribute('secondary', 'rgb(228, 145, 145)');
+        selectDialog.setAttribute('paper', 'rgb(243, 243, 243)');
+        selectDialog.setAttribute('all', config.allowAll ? 'true' : 'false');
+        selectDialog.setAttribute('selected', config.selected);
+        selectDialog.setAttribute('token', config.token);
+        selectDialog.setAttribute('open', 'true');
+        document.body.appendChild(selectDialog);
     });
 }
